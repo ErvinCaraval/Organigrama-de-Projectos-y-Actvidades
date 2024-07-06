679e1fe278cd83d3d3bccc783ca967d169c8ca3e
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-07-06 11:59:52 -0500

    se usa sonar cloudsdsfewwffssssdddwwdddwww

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -26,7 +26,7 @@ class ProyectoSerializer(serializers.ModelSerializer):
             if len(nombre.split()) > 20:
                 raise serializers.ValidationError('El nombre del proyecto no puede exceder 20 palabras.')
 
-        # Validación de descripción
+        # Validación de descripció
         if descripcion:
             if len(descripcion) > 250:
                 raise serializers.ValidationError('La descripción no puede exceder 250 caracteres.')
